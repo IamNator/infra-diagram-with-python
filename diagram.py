c100fa6ab42a3a18ee04af051012ae1700075e06
Nator Verinumbe <natverior1@gmail.com>
2023-04-06 12:16:06 +0000

    update

--- diagram.py
+++ diagram.py
@@ -2,7 +2,7 @@ from diagrams import Diagram, Cluster
 from diagrams.aws.compute import EKS
 from diagrams.onprem.client import User
 from diagrams.aws.network import ELB
-from diagrams.aws.database import ElasticacheForRedis as Redis
+from diagrams.k8s.network import Ing as IngressController
 from diagrams.programming.language import Go as Golang, Typescript
 
 with Diagram("EKS Architecture", show=False):
@@ -10,12 +10,14 @@ with Diagram("EKS Architecture", show=False):
     with Cluster("Backend API service"):
         api_gateway = Typescript("API Gateway")
         golang_services = [
-            Golang("Service 1"),
-            Golang("Service 2"),
-            Golang("Service 3")
+            Golang("Auth Service"),
+            Golang("Company Service"),
+            Golang("Notification Service")
         ]
+        
+        ingress_controller = IngressController("Ingress Controller")
 
     typescript_app = Typescript("Frontend App")
 
-    User("End User") >> ELB("Load Balancer") >> typescript_app >> api_gateway >> golang_services 
-
+    User("End User") >> ELB("Load Balancer") >> ingress_controller >> api_gateway >> golang_services 
+    typescript_app >> api_gateway
